ca5f4148658008dd46dda9f11f32b7f6ce9737b2
r3ahim <r9ahim@gmail.com>
2024-08-23 15:00:52 +0600

    last commit it will be deleted from computer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
 import Hero from './components/Hero/Hero';
+import Plans from './components/Plans/Plans';
 import Programs from './components/Programs/Programs';
+import Reasons from './components/Reasons/Reasons';
+import Testimoniials from './components/Testimonials/Testimoniials';
 function App() {
   return (
     <div className="App">
         <Hero/>
         <Programs/>
+        <Reasons/>
+        <Plans/>
+        <Testimoniials/>
+        <Footer/>
     </div>
   );
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,6 +9,7 @@ import Calories from '../../assets/calories.png'
 const Hero = () => {
   return (
    <div className="hero">
+    <div className="blur hero-blur"></div>
     {/* left side */}
     <div className="left-h">
      <Header/>
